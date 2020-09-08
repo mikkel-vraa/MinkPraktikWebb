9810844bafed4b450fa0911cb77952080902afc4
Chris <manchristoffer@yahoo.dk>
2020-09-08 08:27:59 +0200

    First commit
    First commit

---
+++ ATestWebs/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+dotenv.config({ path: './.env'})
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    //port: 3000,
+    database: process.env.DATABASE,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_password
+    //Trusted_Connection: false
+    //Encrypt=True
+
+    /*host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_password,
+    database: process.env.DATABASE*/
+
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(cookieParser());
+
+console.log(__dirname);
+app.set('view engine', 'hbs');
+
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen("3000", ()=>{
+    console.log("Server started on port 3000");
+})
